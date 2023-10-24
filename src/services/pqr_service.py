9a94974ed0dd87d8fbe7a1a1467921cff2aed7b0
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-10-24 10:06:15 -0500

    we have changed the use of the api for a worse one but it works

---
+++ src/services/pqr_service.py
@@ -0,0 +1,2 @@
+def make_pqr():
+  pass
--- src/services/wit_service.py
+++ src/services/wit_service.py
@@ -2,10 +2,49 @@ from wit import Wit
 from decouple import config
 
 
-client = Wit(config('TOKEN'))
+# client = Wit(config('TOKEN'))
+
+options = {
+  "password": ['cambiar','contraseña','clave','ingresar','problema','quiero'],
+  "pqr" : ['problema','pqr','cambiar','no','funciona','error','quiero','queja','insatisfecho'],
+  "tournament" : ['torneo','problema','ingresar','persona','participante'],
+}
+
+# def give_an_answer(message: str) -> str | None:
+#   res = client.message(message)
+#   if not res['intents'] or res['intents'][0]['name'] == 'no_sense':
+#     return None 
+#   return res['intents'][0]['name']
 
 def give_an_answer(message: str) -> str | None:
-  res = client.message(message)
-  if not res['intents'] or res['intents'][0]['name'] == 'no_sense':
-    return None 
-  return res['intents'][0]['name']
+  answers = message.lower().split(' ')
+    
+
+  best_one = ''
+  contador = 0
+  for clave in options:
+    contador_temp=0
+    for answer in answers:
+      if answer in options[clave]:
+        contador_temp+=1
+    if contador_temp>contador:
+      contador = contador_temp
+      best_one = clave
+  
+  if contador <3:
+    if 'contraseña' in answers or 'clave' in answers: return 'password'
+    if 'pqr' in answers or 'queja' in answers or 'insatisfecho' in answers or 'insatisfecha' in answers: return 'pqr'
+    if 'torneo' in answers: return 'tournament'
+    if len(answers) == 1:
+      if answers[0] == 'si':
+        return 'yes'
+      if answers[0] == 'no':
+        return 'no'
+      return 'no_sense'
+    best_one = 'no_sense'
+  return best_one
+
+
+
+
+
